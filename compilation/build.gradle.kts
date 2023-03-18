b7a233199a7ff12cb45348c6fcc077c1baf1ec46
André L. Santos <andre.santos@iscte.pt>
2023-03-18 12:36:48 +0000

    -  build.gradle.kts with SWT from maven

--- compilation/build.gradle.kts
+++ compilation/build.gradle.kts
@@ -3,24 +3,24 @@ plugins {
 }
 
 group = "pt.iscte.javardise"
-version = "0.1"
+//version = "0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation(project(":"))
 
     implementation("com.github.javaparser:javaparser-symbol-solver-core:3.24.8")
-    val os = System.getProperty("os.name").toLowerCase()
-    if (os.contains("mac")) {
-        implementation(files("../libs/swt-macos.jar"))
-    } else if (os.contains("windows")) {
-        implementation(files("../libs/swt-windows.jar"))
-    }
+//    val os = System.getProperty("os.name").toLowerCase()
+//    if (os.contains("mac")) {
+//        implementation(files("../libs/swt-macos.jar"))
+//    } else if (os.contains("windows")) {
+//        implementation(files("../libs/swt-windows.jar"))
+//    }
 
 }
 
