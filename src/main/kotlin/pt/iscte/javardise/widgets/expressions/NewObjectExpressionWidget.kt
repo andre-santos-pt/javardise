ca92b34be705755af6f169eff8912987be6f37ef
André L. Santos <andre.santos@iscte.pt>
2023-05-23 14:59:27 +0100

    bug fix #5

--- src/main/kotlin/pt/iscte/javardise/widgets/expressions/NewObjectExpressionWidget.kt
+++ src/main/kotlin/pt/iscte/javardise/widgets/expressions/NewObjectExpressionWidget.kt
@@ -4,11 +4,13 @@ import com.github.javaparser.ast.expr.Expression
 import com.github.javaparser.ast.expr.ObjectCreationExpr
 import com.github.javaparser.ast.observer.ObservableProperty
 import com.github.javaparser.ast.type.ClassOrInterfaceType
+import org.eclipse.swt.SWT
 import org.eclipse.swt.widgets.Composite
 import pt.iscte.javardise.Configuration
 import pt.iscte.javardise.SimpleTypeWidget
 import pt.iscte.javardise.basewidgets.TextWidget
 import pt.iscte.javardise.basewidgets.TokenWidget
+import pt.iscte.javardise.external.empty
 import pt.iscte.javardise.external.isValidClassType
 
 // anonymous class body
@@ -29,13 +31,20 @@ class NewObjectExpressionWidget(
         }
 
         keyword = newKeywordWidget(this, "new")
+        keyword.addKeyEvent(SWT.BS) {
+            editEvent(null)
+        }
+
         id = SimpleTypeWidget(this, node.type)
         id.addFocusLostAction(::isValidClassType) {
             node.modifyCommand(node.typeAsString, it, node::setType)
         }
+        id.addDeleteEmptyListener {
+            editEvent(null)
+        }
 
         args = ExpressionListWidget(this, "(", ")", this, node.arguments) {
-            editEvent(Configuration.hole())
+            id.setFocus()
         }
 
         observeNotNullProperty<ClassOrInterfaceType>(ObservableProperty.TYPE) {
@@ -44,7 +53,7 @@ class NewObjectExpressionWidget(
     }
 
     override fun setFocusOnCreation(firstFlag: Boolean) {
-        setFocus()
+        args.setFocus()
     }
 
     override val head: TextWidget
